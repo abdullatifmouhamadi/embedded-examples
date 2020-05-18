01480ae518f50ed870145b3db72af1b5968d5125
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-05-18 14:11:27 +0000

    example 1 works

---
+++ 1-test-rf24-drivers-with-rpi3/1-check/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: ABDULLATIF Mouhamadi <bourou01dev@gmail.com>
+ *
+ * License: MIT
+ *
+ * Test de prise en charge du module par la bibliothèque RF24 (si connecté correctement)
+ */
+#include <iostream>
+#include <RF24/RF24.h>
+
+using namespace std;
+
+#define PIN_CE 17
+#define PIN_CSN 0
+
+int main() {
+
+	RF24 radio(PIN_CE, PIN_CSN);
+	radio.begin();
+
+	if (radio.isPVariant()) {
+		cout << "The module is supported" << endl;
+		radio.printDetails(); 
+	}
+	else {
+		cout << "Unknown module" << endl;
+	}
+}
--- 1-test-rf24-drivers-with-rpi3/README.md
+++ 1-test-rf24-drivers-with-rpi3/README.md
@@ -1,4 +1,12 @@
 # broches
 
 
+https://www.hackster.io/wirekraken/connecting-an-nrf24l01-to-raspberry-pi-9c0a57
+https://github.com/abdullatifmouhamadi/Raspberry-Pi-NRF24
+
+
+
+
+
+
 
