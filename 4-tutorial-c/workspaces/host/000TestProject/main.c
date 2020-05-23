5310afffbb0aedfc513f7bdb6d66d8d8b71f5534
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-05-23 21:11:03 +0200

    Adding workspaces

---
+++ 4-tutorial-c/workspaces/host/000TestProject/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: 23 mai 2020
+ *      Author: abdullatif
+ */
+
+
+#include<stdio.h>
+
+int main(void) {
+	printf("Hello world\n");
+
+	printf("Press 'Enter' to exit this application\n");
+
+	getchar();
+
+	return 0;
+}
