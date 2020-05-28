ea9b2f2ccda7952c5d407677527ec3ffed99d287
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-05-28 17:30:16 +0200

    adding fold

---
+++ 4-tutorial-c/3-dummy/1-define-variable/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+		/* définition d'une variable */
+    unsigned char maVariable; 			
+    /* initialisation d'une variable */ 
+    maVariable = 65;          			 
+    /* affichage de la variable 'maVariable' de type 'char' */
+    printf("ma variable vaut '%c'\n", maVariable);
+}
